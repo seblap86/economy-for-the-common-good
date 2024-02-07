3a70c97b5add148dc8c7e06cde4e7532059deb58
Sebastian Bobeth <sebastian.bobeth@posteo.de>
2024-02-07 17:59:39 +0100

    Update ecg_database_queries.sql

--- analysis/ecg_database_queries.sql
+++ analysis/ecg_database_queries.sql
@@ -1,5 +1,60 @@
 use ecg;
 
+### VIEWS
+# Highscores? ;)
+CREATE OR REPLACE VIEW highscores AS
+SELECT
+	o.organization,
+    c.country_name,
+    b.entry_number,
+    b.year,
+	a.audit_type,
+    b.score
+FROM organizations AS o
+	INNER JOIN balances AS b
+		ON o.id = b.organizations_id
+	INNER JOIN balance_audit_types AS a
+		ON a.id = b.audit_types_id
+	INNER JOIN locations AS l
+		ON l.id = o.locations_id
+	INNER JOIN countries AS c
+		ON c.id = l.country_id
+ORDER BY b.score DESC
+LIMIT 100;
+
+# Lowscores? ;)
+CREATE OR REPLACE VIEW lowscores AS
+SELECT
+	o.organization,
+    c.country_name,
+    b.entry_number,
+    b.year,
+	a.audit_type,
+    b.score
+FROM organizations AS o
+	INNER JOIN balances AS b
+		ON o.id = b.organizations_id
+	INNER JOIN balance_audit_types AS a
+		ON a.id = b.audit_types_id
+	INNER JOIN locations AS l
+		ON l.id = o.locations_id
+	INNER JOIN countries AS c
+		ON c.id = l.country_id
+WHERE score IS NOT NULL
+ORDER BY b.score ASC
+LIMIT 100;
+
+### EXPLORATION
+# How was the development within companies (year & score of each balance)?
+SELECT
+	o.organization,
+    b.entry_number,
+    b.year,
+    b.score
+FROM organizations AS o
+	INNER JOIN balances AS b
+		ON o.id = b.organizations_id;
+
 # Large companies from Germany and how many balances they have in their records?
 SELECT 
 	o.organization,
